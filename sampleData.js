ab8fe4f9e279623435cb2b94ad103f338bf76126
Paul Kang <paul@paul-kang.com>
2020-04-05 23:31:20 +0800

    WIP

--- sampleData.js
+++ sampleData.js
@@ -1,7 +1,10 @@
 var sample_data = { 
   "User_ID":"000123", 
   "User_Name":"David Chang",
-  "FFM":12.3,   "FM":8.76,
+  "FFM": 12.3,   
+  "FM":  8.76,
+  "HS":  72.4,
+  "BMI": 21.5,
   "Proteint_Percent" : 2.4,  
   "FM_Percent":10.1 
 }
