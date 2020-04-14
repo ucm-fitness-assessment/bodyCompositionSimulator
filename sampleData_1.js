15ca49ea113c998145ad5f8f063ce6eb1740a7fc
Paul Kang <paul@paul-kang.com>
2020-04-14 22:36:53 +0800

    V0.2 add post response

---
+++ sampleData_1.js
@@ -0,0 +1,371 @@
+var sample_data_1 = {
+
+        "AC": 66.4,
+
+        "AMC": 22.1,
+
+        "APP_Version": "u.1.0.0.53",
+
+        "Age": 20.66,
+
+        "BCM": 25.2,
+
+        "BIVA_X": -1.1,
+
+        "BIVA_Y": -3.1,
+
+        "BMC": 2.3,
+
+        "BMCP": 5.2,
+
+        "BMI": 17.5,
+
+        "BMINRL": 18.5,
+
+        "BMINRU": 24.9,
+
+        "BMIOU": 29.9,
+
+        "BMITRL": 10.0,
+
+        "BMITRU": 55.0,
+
+        "BMR": 1228.0,
+
+        "Device_IP": "192.168.50.123",
+
+        "Device_Mac": "40:9F:38:4E:C8:DB",
+
+        "ECW": 11.6,
+
+        "ECWL": 10.4,
+
+        "ECWP": 26.1,
+
+        "ECWU": 12.8,
+
+        "EI": 0.414,
+
+        "Ethnic": 0,
+
+        "FFM": 39.8,
+
+        "FFM_Index": 15.5,
+
+        "FM": 4.9,
+
+        "FMNL": 8.0,
+
+        "FMNRL": 80.0,
+
+        "FMNRU": 160.0,
+
+        "FMNU": 12.5,
+
+        "FMTRL": 50.0,
+
+        "FMTRU": 400.0,
+
+        "FM_Index": 1.9,
+
+        "Gender": 0,
+
+        "HS": 81.3,
+
+        "Height": 160.0,
+
+        "ICW": 16.5,
+
+        "ICWL": 17.0,
+
+        "ICWP": 36.9,
+
+        "ICWU": 20.8,
+
+        "ILA100": 323.6,
+
+        "ILA20": 327.3,
+
+        "ILA250": 310.2,
+
+        "ILA5": 327.4,
+
+        "ILA50": 326.2,
+
+        "ILL100": 265.8,
+
+        "ILL20": 269.0,
+
+        "ILL250": 253.8,
+
+        "ILL5": 269.1,
+
+        "ILL50": 268.1,
+
+        "ILW100": 625.1,
+
+        "ILW20": 631.5,
+
+        "ILW250": 602.9,
+
+        "ILW5": 631.8,
+
+        "ILW50": 629.8,
+
+        "IRA100": 322.9,
+
+        "IRA20": 329.3,
+
+        "IRA250": 302.5,
+
+        "IRA5": 329.5,
+
+        "IRA50": 327.6,
+
+        "IRL100": 266.8,
+
+        "IRL20": 268.7,
+
+        "IRL250": 258.5,
+
+        "IRL5": 268.9,
+
+        "IRL50": 268.1,
+
+        "IRW100": 624.4,
+
+        "IRW20": 633.1,
+
+        "IRW250": 597.2,
+
+        "IRW5": 633.5,
+
+        "IRW50": 630.8,
+
+        "LAC": 23.3,
+
+        "LAFM": 0.3,
+
+        "LAFMNRL": 0.3,
+
+        "LAFMNRU": 0.5,
+
+        "LALM": 2.0,
+
+        "LALMNRL": 1.6,
+
+        "LALMNRU": 2.4,
+
+        "LALM_Sco": 1.5,
+
+        "LHGF": 206.0,
+
+        "LHGFL": 185.0,
+
+        "LHGFU": 226.0,
+
+        "LLFM": 1.4,
+
+        "LLFMNRL": 1.6,
+
+        "LLFMNRU": 2.3,
+
+        "LLLM": 5.7,
+
+        "LLLMNRL": 5.7,
+
+        "LLLMNRU": 8.6,
+
+        "LLLM_Sco": 1.0,
+
+        "LM": 37.4,
+
+        "LM_Sco": 1.3,
+
+        "MineralL": 1.5,
+
+        "MineralU": 2.5,
+
+        "Model": "MA801",
+
+        "PBF": 11.1,
+
+        "PBFNRL": 18.0,
+
+        "PBFNRU": 28.0,
+
+        "PBFTRL": 8.0,
+
+        "PBFTRU": 60.0,
+
+        "PM": 9.3,
+
+        "PMP": 0.0,
+
+        "PhLA100": 7.7,
+
+        "PhLA20": 1.3,
+
+        "PhLA250": 16.7,
+
+        "PhLA5": -0.0,
+
+        "PhLA50": 3.8,
+
+        "PhLL100": 8.7,
+
+        "PhLL20": 1.6,
+
+        "PhLL250": 18.6,
+
+        "PhLL5": 0.2,
+
+        "PhLL50": 4.4,
+
+        "PhLW100": 6.8,
+
+        "PhLW20": 0.8,
+
+        "PhLW250": 15.1,
+
+        "PhLW5": 0.1,
+
+        "PhLW50": 3.1,
+
+        "PhRA100": 11.0,
+
+        "PhRA20": 2.0,
+
+        "PhRA250": 23.2,
+
+        "PhRA5": 0.1,
+
+        "PhRA50": 5.5,
+
+        "PhRL100": 5.9,
+
+        "PhRL20": 1.1,
+
+        "PhRL250": 12.8,
+
+        "PhRL5": 0.1,
+
+        "PhRL50": 2.9,
+
+        "PhRW100": 7.3,
+
+        "PhRW20": 1.0,
+
+        "PhRW250": 15.8,
+
+        "PhRW5": 0.1,
+
+        "PhRW50": 3.4,
+
+        "ProteinL": 6.8,
+
+        "ProteinU": 8.4,
+
+        "RAC": 22.0,
+
+        "RAFM": 0.3,
+
+        "RAFMNRL": 0.3,
+
+        "RAFMNRU": 0.5,
+
+        "RALM": 2.0,
+
+        "RALMNRL": 1.6,
+
+        "RALMNRU": 2.4,
+
+        "RALM_Sco": 1.5,
+
+        "RHGF": 227.0,
+
+        "RHGFL": 205.0,
+
+        "RHGFU": 250.0,
+
+        "RLFM": 1.5,
+
+        "RLFMNRL": 1.6,
+
+        "RLFMNRU": 2.3,
+
+        "RLLM": 5.9,
+
+        "RLLMNRL": 5.7,
+
+        "RLLMNRU": 8.6,
+
+        "RLLM_Sco": 1.1,
+
+        "Report_Date": "2020-04-13 17:18:00",
+
+        "SAT": 43.3,
+
+        "SMM": 21.7,
+
+        "SMMNRL": 90.0,
+
+        "SMMNRU": 110.0,
+
+        "SMMTRL": 60.0,
+
+        "SMMTRU": 180.0,
+
+        "SMM_Index": 8.5,
+
+        "Serial_No": "T18000001",
+
+        "TBW": 28.1,
+
+        "TBW_Div_FFM": 70.8,
+
+        "TFM": 0.5,
+
+        "TFMNRL": 4.2,
+
+        "TFMNRU": 6.3,
+
+        "TLM": 17.1,
+
+        "TLMNRL": 14.7,
+
+        "TLMNRU": 22.0,
+
+        "TLM_Sco": 1.3,
+
+        "User_ID": "09XX654321",
+
+        "User_Name": "小B",
+
+        "VFA": 0.1,
+
+        "WHR": 0.8,
+
+        "WHRNRL": 0.75,
+
+        "WHRNRU": 0.85,
+
+        "WHRTRL": 0.6,
+
+        "WHRTRU": 1.2,
+
+        "WLMNRL": 31.8,
+
+        "WLMNRU": 47.8,
+
+        "WNRL": 85.0,
+
+        "WNRU": 115.0,
+
+        "WTRL": 50.0,
+
+        "WTRU": 200.0,
+
+        "Weight": 44.7
+
+    };
